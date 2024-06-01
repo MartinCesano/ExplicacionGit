951aedca99ae6e602583b8cb3cb3055e7e01265b
ValeChiapero <v08.03.00@gmail.com>
2024-06-01 11:43:51 -0300

    Se solucionaron los conflictos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,7 +8,6 @@ import { IProductEntity } from "../../DataBases/interfaces.db";
 @Controller('products')
 export class ProductsController {
     constructor(private service: ProductsService) {}
-<<<<<<< HEAD
     @Post()
     async createProduct(@Body() product: DeepPartial<ProductEntity>): Promise<IProductEntity>{
         return await this.service.createProduct(product);
@@ -19,7 +18,6 @@ export class ProductsController {
         return await this.service.findProducts();
     }
 
-=======
     @Get(':id')
     async findProductById(@Param('id') id: number): Promise<IProductEntity>{
         return await this.service.findProductById(id);
@@ -28,7 +26,4 @@ export class ProductsController {
     async updateProductById(@Param('id') id: number, @Body() bodyUpdateProduct: Partial<ProductEntity>): Promise<IProductEntity>{
         return await this.service.updateProductById(id, bodyUpdateProduct);
     }
-
-    
->>>>>>> 3e0686d8216f9ca32c5113f32cd06d061077b6b0
 }
